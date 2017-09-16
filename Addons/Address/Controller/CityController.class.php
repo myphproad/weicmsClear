592844eecbf429cc4c6310a7fdf4f145718eca54
like0811 <1170503989@qq.com>
2017-09-16 13:27:31 +0800

    city

---
+++ Addons/Address/Controller/CityController.class.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Addons\Address\Controller;
+use Home\Controller\AddonsController;
+
+class CityController extends AddonsController{
+
+    function _initialize()
+    {
+        $this->model = $this->getModel('city');
+        parent::_initialize();
+    }
+    /**
+     * @author:like
+     * @remark:城市站点;
+     * @date:2017年9月16日
+     * @returnType: Json
+     */
+    public function lists(){
+        //ID 站点名称 站点拼音 是否开启
+        $list_data = $this->_get_model_list($this->model);
+        echo M()->_sql();
+        dump($list_data);die();
+        $map['token']  = get_token();
+        $this->assign($list_data);
+        $this->display();
+    }
+}
